b7784c0708322f65b002f85910d86968872176f8
woojinv <oh.woojin95@gmail.com>
2024-02-02 22:32:35 -0600

    delete answers

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,17 +14,7 @@
 */
 
 function insertionSort(nums) {
-  for (let i = 1; i < nums.length; i++) {
-    const current = nums[i];
-    let j;
-    for (j = i - 1; j >= 0 && current < nums[j]; j--) {
-      nums[j + 1] = nums[j];
-    }
 
-    nums[j + 1] = current;
-  }
-
-  return nums;
 }
 
 // unit tests
