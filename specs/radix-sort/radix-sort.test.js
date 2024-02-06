b2a155697c897274ec42478f21df8ec6a979d44f
woojinv <oh.woojin95@gmail.com>
2024-02-05 21:30:49 -0600

    pass 1/2 test cases

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -10,66 +10,49 @@
 */
 
 function radixSort(array) {
-  // code goes here
+  // get number of iterations for loop from input array.
+  const maxNumDigits = Math.max(...array).toString().length;
+
+  // create loop based on retrieved number of iterations
+  for (let i = 0; i < maxNumDigits; i++) {
+    const buckets = [[], [], [], [], [], [], [], [], [], []];
+    for (let j = 0; j < array.length; j++) {
+      // from each item, get value of current significant digit.
+      const currentSigDigit = getCurrentSignificantDigitValue(array[j], i);
+      // place item in corresponding bucket.
+      buckets[currentSigDigit].push(array[j]);
+    }
+
+    // clear array before pushing items into it.
+    array = [];
+    // dequeue items from buckets
+    for (let k = 0; k < buckets.length; k++) {
+      array.push(...buckets[k]);
+    }
+  }
+
+  // pass through array again.
+  return array;
+}
+
+function getCurrentSignificantDigitValue(num, significantDigit) {
+  const { length } = num.toString();
+  const value = Number(num.toString()[length - 1 - significantDigit]);
+
+  return Number.isNaN(value) ? 0 : value;
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("radix sort", function () {
-  it("should sort correctly", () => {
-    const nums = [
-      20,
-      51,
-      3,
-      801,
-      415,
-      62,
-      4,
-      17,
-      19,
-      11,
-      1,
-      100,
-      1244,
-      104,
-      944,
-      854,
-      34,
-      3000,
-      3001,
-      1200,
-      633
-    ];
+describe('radix sort', function () {
+  it('should sort correctly', () => {
+    const nums = [20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34, 3000, 3001, 1200, 633];
     const ans = radixSort(nums);
-    expect(ans).toEqual([
-      1,
-      3,
-      4,
-      11,
-      17,
-      19,
-      20,
-      34,
-      51,
-      62,
-      100,
-      104,
-      415,
-      633,
-      801,
-      854,
-      944,
-      1200,
-      1244,
-      3000,
-      3001
-    ]);
+    expect(ans).toEqual([1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944, 1200, 1244, 3000, 3001]);
   });
-  it("should sort 99 random numbers correctly", () => {
+  it('should sort 99 random numbers correctly', () => {
     const fill = 99;
-    const nums = new Array(fill)
-      .fill()
-      .map(() => Math.floor(Math.random() * 500000));
+    const nums = new Array(fill).fill().map(() => Math.floor(Math.random() * 500000));
     const ans = radixSort(nums);
     expect(ans).toEqual(nums.sort());
   });
