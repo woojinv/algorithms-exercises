730387dcce612a4e77302f4d4c04390769a7c61e
woojinv <oh.woojin95@gmail.com>
2024-02-09 23:25:43 -0600

    continue working on getDigitValue()

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -43,23 +43,31 @@ function getLengthOfLongestNumber(nums) {
   return length;
 }
 
-function getDigitValue(num, currentDigit, lengthOfLongestNumber) {
-  // left pad number with 0s to be the lengthOfLongestNumber so that numbers shorter than that can still be accessed using the index.
-  
-  const digitValue = num.toString()
-} 
+function getDigitValue(num, currentDigit) {
+  // convert digit to string.
+  // access digit at index of current digit - 1.
+  return num.toString()[currentDigit - 1] || 0;
+}
 
 // unit tests
 // do not modify the below code
-describe('radix sort', function () {
-  it('should sort correctly', () => {
-    const nums = [20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34, 3000, 3001, 1200, 633];
+describe("radix sort", function () {
+  it("should sort correctly", () => {
+    const nums = [
+      20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
+      3000, 3001, 1200, 633,
+    ];
     const ans = radixSort(nums);
-    expect(ans).toEqual([1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944, 1200, 1244, 3000, 3001]);
+    expect(ans).toEqual([
+      1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944,
+      1200, 1244, 3000, 3001,
+    ]);
   });
-  it('should sort 99 random numbers correctly', () => {
+  it("should sort 99 random numbers correctly", () => {
     const fill = 99;
-    const nums = new Array(fill).fill().map(() => Math.floor(Math.random() * 500000));
+    const nums = new Array(fill)
+      .fill()
+      .map(() => Math.floor(Math.random() * 500000));
     const ans = radixSort(nums);
     expect(ans).toEqual(nums.sort());
   });
