b495398f1b85a70bb4ca4dbb9896485220eca3f7
woojinv <oh.woojin95@gmail.com>
2024-02-11 16:37:03 -0600

    reset

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -10,46 +10,7 @@
 */
 
 function radixSort(array) {
-  const largestNumberLength = getLargestNumberLength(array);
-
-  // create 10 buckets for digits numbers 0-9.
-  const buckets = new Array(10).fill().map(() => []);
-
-  // loop through array.
-  // the number of digits in longest item is how many times to loop.
-  for (let currentSigDigIndex = largestNumberLength - 1; currentSigDigIndex >= 0; currentSigDigIndex--) {
-    while (array.length) {
-      const current = array.shift();
-      // track which significant digit you are currently sorting by.
-      // retrieve the value of that sig digit for the current iteration.
-      const valueAtSigDig = getValueAtSigDig(current, currentSigDigIndex, largestNumberLength);
-
-      // put that entire item into the bucket for its corresponding sig digit value.
-      buckets[valueAtSigDig].push(current);
-    }
-
-    // once all item from original array are in the buckets,
-    // empty the items in the buckets into the original array,
-    // maintaining their order in the buckets, from 0-9.
-    for (let k = 0; k < buckets.length; k++) {
-      while (buckets[k].length) {
-        array.push(buckets[k].shift());
-      }
-    }
-  }
-  // once this process is repeated enough times, return the sorted array.
-
-  return array;
-}
-
-function getLargestNumberLength(nums) {
-  let largestNumber = 0;
-  for (let i = 0; i < nums.length; i++) {
-    const currentNum = nums[i];
-    if (currentNum > largestNumber) largestNumber = currentNum;
-  }
-
-  return largestNumber.toString().length;
+  
 }
 
 function getValueAtSigDig(currentNumber, currentSigDigIndex, largestNumberLength) {
